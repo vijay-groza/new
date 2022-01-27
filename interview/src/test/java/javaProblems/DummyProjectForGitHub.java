c23ae8eaec9094db21bd24f5037322574106e011
vijay <vj192152@gmail.com>
2022-01-27 16:14:40 +0530

    to verify the third statement

--- interview/src/test/java/javaProblems/DummyProjectForGitHub.java
+++ interview/src/test/java/javaProblems/DummyProjectForGitHub.java
@@ -4,7 +4,9 @@ public class DummyProjectForGitHub {
 
 	public static void main(String[] args) {
 		System.out.println("dummy project");
-		System.out.println("this change is made in git hub edit section and it is tested whether it will be visible in eclipse")
+		System.out.println("this change is made in git hub edit section and it is tested whether it will be visible in eclipse");
+		
+		System.out.println("i think the second print statement is postive and it is checked from the eclipse");
 
 	}
 
