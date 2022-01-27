d12b2eaa5de2e8a13a6fe5f222768bce34e90535
vijay-groza <98517259+vijay-groza@users.noreply.github.com>
2022-01-27 16:10:27 +0530

    Update DummyProjectForGitHub.java
    an update made in the sysout secltion

--- interview/src/test/java/javaProblems/DummyProjectForGitHub.java
+++ interview/src/test/java/javaProblems/DummyProjectForGitHub.java
@@ -4,6 +4,7 @@ public class DummyProjectForGitHub {
 
 	public static void main(String[] args) {
 		System.out.println("dummy project");
+		System.out.println("this change is made in git hub edit section and it is tested whether it will be visible in eclipse")
 
 	}
 
